f32094801de6e71c1010f62db04ad8a729b9390b
Aniket Awasthi <paradoxplayz95@gmail.com>
2025-09-25 16:47:04 +0530

    Added The firestore Data Base for Internships

--- src/firebase/firestore.ts
+++ src/firebase/firestore.ts
@@ -4,12 +4,13 @@ import { FirebaseOptions } from "firebase/app";
 
 // 🔹 Replace with your Firebase config
 const firebaseConfig: FirebaseOptions = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MSG_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: "AIzaSyCYWBtXJm2tadLw9-XTR78161as3z95kqw",
+  authDomain: "governmentinternaimatch.firebaseapp.com",
+  projectId: "governmentinternaimatch",
+  storageBucket: "governmentinternaimatch.firebasestorage.app",
+  messagingSenderId: "305179289696",
+  appId: "1:305179289696:web:e1bced3e12ec99d502fb05",
+  measurementId: "G-JTW9NG935E"
 };
 
 const app = initializeApp(firebaseConfig);
